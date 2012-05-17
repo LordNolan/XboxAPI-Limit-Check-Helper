ee516a331289e4ec75627849a6eaf782dd48cfc8
Alan Wynn <wynn_45@hotmail.com>
2012-05-17 11:22:10 +0100

    add README file

---
+++ README.php
@@ -0,0 +1,11 @@
+XboxAPI - Limit Check
+
+This is some example code for useage with the XboxAPI,
+it will help you with the API limit within your code.
+
+@package 			XboxAPI Limit Check
+@author 			Alan Wynn (djekl)
+@copyright 			Copyright (c) 2012, djekl Developments.
+@license 			MIT - https://xboxapi.com/licenses/MIT
+@link 				https://xboxapi.com
+@since 				Version 1.0
